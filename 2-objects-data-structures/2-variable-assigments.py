ffc9668c58350b987c76262fa0d9bd59557e6e2e
davidokun <davidokun99@gmail.com>
2018-05-08 18:41:36 -0500

    Variable Assignments

---
+++ 2-objects-data-structures/2-variable-assigments.py
@@ -0,0 +1,19 @@
+a = 5
+print(a)
+
+a = 10
+print(a)
+
+a = a + a
+print(a)
+
+print(type(a))
+
+a = 30.1
+print(type(a))
+
+myIncome = 100
+taxRate = 0.1
+
+myTaxes = myIncome * taxRate
+print(myTaxes)
