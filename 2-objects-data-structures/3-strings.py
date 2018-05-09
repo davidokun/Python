3667001dd459b13732967bae6e44e949e5f3ae3e
davidokun <davidokun99@gmail.com>
2018-05-08 22:20:38 -0500

    Indexing and Slicing with Strings.

--- 2-objects-data-structures/3-strings.py
+++ 2-objects-data-structures/3-strings.py
@@ -16,3 +16,34 @@ print("Hello \tworld")
 
 # Length of a String
 print(len("I am Iron Man"))
+
+# INDEXING
+myString = "Hello World"
+print(myString[0])
+print(myString[8])
+
+# Negative indexing
+print(myString[-1])
+print(myString[-5])
+
+# SLICING
+myString = "abcdefghijk"
+print(myString)
+
+# myString[start:stop:step]
+# From 2 to end
+print(myString[2:])
+
+# From Beginning to 3 - Not inclusive
+print(myString[:3])
+
+print(myString[3:6])
+
+# Using step
+print(myString[::2])
+
+print(myString[2:7:2])
+
+# Reversing a string with step
+print(myString[::-1])
+
