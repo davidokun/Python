95d052a253f164251549c0b2b59927cdf779562a
davidokun <davidokun99@gmail.com>
2018-05-08 22:02:51 -0500

    Introduction to Strings.

---
+++ 2-objects-data-structures/3-strings.py
@@ -0,0 +1,18 @@
+# Single quotes
+print('hello')
+
+# Double quotes
+print("World")
+
+# Whole string
+print("This is a phrase")
+
+# Double and Single quotes
+print(" I'm on a run")
+
+# Special
+print("Hello \nworld")
+print("Hello \tworld")
+
+# Length of a String
+print(len("I am Iron Man"))
