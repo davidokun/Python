4c2cd05790f02ad88885c823ed4b931e440af112
davidokun <davidokun99@gmail.com>
2018-05-10 08:07:47 -0500

    Sets in Python.

---
+++ 2-objects-data-structures/7-sets.py
@@ -0,0 +1,12 @@
+# Unordered collection of unique elements
+mySet = set()
+print(mySet)
+
+mySet.add(1)
+mySet.add(2)
+print(mySet)
+
+# Unique values from a list
+myList = [1, 2, 2, 3, 4, 5, 5, 5]
+newSet = set(myList)
+print(newSet)
