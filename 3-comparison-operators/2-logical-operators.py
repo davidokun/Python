66b9c9f15b9f191d5a1ce30e48149e39dfe7ee09
davidokun <davidokun99@gmail.com>
2018-05-11 09:27:06 -0500

    Chaining Comparison Operators in Python with Logical Operators.

---
+++ 3-comparison-operators/2-logical-operators.py
@@ -0,0 +1,15 @@
+print(1 < 2 < 3)
+
+# Logical AND
+print(1 < 2 and 2 < 3)
+
+print('h' == 'h' and 2 == 2)
+
+# Logical OR
+print(1 == 1 or 2 == 2)
+print(100 == 1 or 2 == 200)
+
+# Logical NOT
+print(not 1 == 1)
+print(not(400 > 500))
+
