1bcefdfce2fdeb94ff335cced482c81af5e51991
davidokun <davidokun99@gmail.com>
2018-05-11 09:15:06 -0500

    Comparison Operators in Python.

---
+++ 3-comparison-operators/1-comparison-operators.py
@@ -0,0 +1,27 @@
+# Equality
+print(2 == 2)
+print(1 == 2)
+print('hello' == 'bye')
+print('Bye' == 'bye')
+
+# Not Equality
+print(3 != 3)
+print(4 != 5)
+
+# Greater than
+print(1 > 3)
+print(5 > 2)
+
+# Less than
+print(30 < 10)
+print(5 < 7)
+
+# Greater than or equal
+print(3 >= 3)
+print(4 >= 3)
+print(1 >= 3)
+
+# Less than or equal
+print(3 <= 3)
+print(4 <= 3)
+print(1 <= 3)
