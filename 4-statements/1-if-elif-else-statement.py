37c7c8fcfe67e07bca7e001dc9a4630f727c69be
davidokun <davidokun99@gmail.com>
2018-05-15 08:35:54 -0500

    If Elif and Else Statements in Python.

---
+++ 4-statements/1-if-elif-else-statement.py
@@ -0,0 +1,35 @@
+if True:
+    print('Its true')
+
+if 3 > 2:
+    print('Its true')
+
+hungry = True
+
+if hungry:
+    print('Feed me!')
+else:
+    print('Not hungry')
+
+# ELSE IF
+
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Bank':
+    print('Money is cool!')
+elif loc == 'Store':
+    print('Welcome to the store!')
+else:
+    print('I do not know much')
+
+name = 'Sammy'
+
+if name == 'Frankie':
+    print('Hello {}!'.format(name))
+elif name == 'Sammy':
+    print('Hello {}!'.format(name))
+else:
+    print('Whats your name?')
+
