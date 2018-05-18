de82fae2f6babfe1d8896b4bc5760e3029aa117d
davidokun <davidokun99@gmail.com>
2018-05-18 08:07:23 -0500

    exercises - ANIMAL CRACKERS

--- 5-methods-and-functions/4-functions-exercises.py
+++ 5-methods-and-functions/4-functions-exercises.py
@@ -1,7 +1,7 @@
 # LESSER OF TWO EVENS
 # Write a function that returns the lesser of two given numbers if both numbers are even,
 # but returns the greater if one or both numbers are odd
-print('# LESSER OF TWO EVENS')
+print('## LESSER OF TWO EVENS: ')
 
 
 def lesser_of_two_evens(a, b):
@@ -19,3 +19,18 @@ def lesser_of_two_evens(a, b):
 
 print(lesser_of_two_evens(2, 4))
 print(lesser_of_two_evens(2, 5))
+
+# ANIMAL CRACKERS:
+# Write a function takes a two-word string and returns True if both words begin with same letter
+print('## ANIMAL CRACKERS:')
+
+
+def animal_crackers(animal):
+    words = animal.split()
+    return words[0][0] == words[1][0]
+
+
+print(animal_crackers('Levelheaded Llama'))
+print(animal_crackers('Crazy Kangaroo'))
+print(animal_crackers('Krazy Karma'))
+
