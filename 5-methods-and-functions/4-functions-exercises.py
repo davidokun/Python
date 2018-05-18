eff6d57310aa417f1ffa7feb8283132fc5154c01
davidokun <davidokun99@gmail.com>
2018-05-18 08:13:11 -0500

    exercises - MAKES TWENTY

--- 5-methods-and-functions/4-functions-exercises.py
+++ 5-methods-and-functions/4-functions-exercises.py
@@ -34,3 +34,21 @@ print(animal_crackers('Levelheaded Llama'))
 print(animal_crackers('Crazy Kangaroo'))
 print(animal_crackers('Krazy Karma'))
 
+
+# MAKES TWENTY:
+# Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
+print('## MAKES TWENTY: ')
+
+
+def makes_twenty(n1, n2):
+    if n1 == 20 or n2 == 20:
+        return True
+    else:
+        return n1 + n2 == 20
+
+
+print(makes_twenty(20, 10))
+print(makes_twenty(12, 8))
+print(makes_twenty(2, 3))
+print(makes_twenty(30, 2))
+print(makes_twenty(30, 20))
