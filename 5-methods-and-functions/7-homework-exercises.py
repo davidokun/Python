78e55fa22bf8992716f94547d317a1b19abaedd4
davidokun <davidokun99@gmail.com>
2018-05-24 09:22:35 -0500

    homework - number is in a given range

--- 5-methods-and-functions/7-homework-exercises.py
+++ 5-methods-and-functions/7-homework-exercises.py
@@ -9,3 +9,21 @@ def vol(rad):
 
 print(vol(2))
 print(vol(8))
+
+# Write a function that checks whether a number is in a given range (inclusive of high and low)
+print("\n# NUMBER IS IN RANGE\n")
+
+
+def ran_check(num, low, high):
+    if num in range(low, high + 1):
+        return '{0} is in the range between {1} and {2}'.format(num, low, high)
+    else:
+        return '{0} is NOT in the range between {1} and {2}'.format(num, low, high)
+
+
+print(ran_check(5, 2, 7))
+print(ran_check(3, 1, 10))
+print(ran_check(10, 1, 10))
+print(ran_check(1, 1, 10))
+print(ran_check(50, 15, 30))
+print(ran_check(0, 15, 31))
