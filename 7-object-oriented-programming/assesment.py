acbca4f03310e2df976e5501f07f0508af0d6f6c
davidokun <davidokun99@gmail.com>
2018-06-06 08:03:01 -0500

    opp-assesment: Implementing distance and slope methods

--- 7-object-oriented-programming/assesment.py
+++ 7-object-oriented-programming/assesment.py
@@ -1,3 +1,8 @@
+import math
+
+print("# CLASS LINE\n")
+
+
 class Line:
 
     def __init__(self, coor1, coor2):
@@ -5,10 +10,10 @@ class Line:
         self.coor2 = coor2
 
     def distance(self):
-        pass
+        return math.sqrt((self.coor2[0] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[1])**2)
 
     def slope(self):
-        pass
+        return (self.coor2[1] - self.coor1[1]) / (self.coor2[0] - self.coor1[0])
 
 
 coordinate1 = (3, 2)
@@ -16,5 +21,5 @@ coordinate2 = (8, 10)
 
 line = Line(coordinate1, coordinate2)
 
-print("Distance is {}".format(line.distance()))
-print("Slope is {}".format(line.slope()))
+print("Distance between {} and {} is {}".format(coordinate1, coordinate2, line.distance()))
+print("Slope between {} and {} is {}".format(coordinate1, coordinate2, line.slope()))
