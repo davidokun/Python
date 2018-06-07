4c7d75a80540e4123212d94930a23fe470165250
davidokun <davidokun99@gmail.com>
2018-06-07 08:34:52 -0500

    opp-exercise: Completing exercise.

---
+++ 7-object-oriented-programming/6-exercise.py
@@ -0,0 +1,28 @@
+class Account:
+
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print("Deposit Accepted")
+
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print("Funds Unavailable!")
+        else:
+            self.balance = self.balance - amount
+            print("Withdrawal Accepted")
+
+    def __str__(self):
+        return "Account owner: {}\nAccount balance: {}".format(self.owner, self.balance)
+
+
+acct1 = Account("Jose", 100)
+print(acct1)
+print(acct1.owner)
+print(acct1.balance)
+acct1.deposit(50)
+acct1.withdraw(75)
+acct1.withdraw(500)
