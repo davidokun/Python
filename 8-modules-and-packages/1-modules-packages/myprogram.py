a99557838a4d5e8d29dcbf7fb18a47998faa739f
davidokun <davidokun99@gmail.com>
2018-06-07 11:10:30 -0500

    Modules and Packages definition and import

---
+++ 8-modules-and-packages/1-modules-packages/myprogram.py
@@ -0,0 +1,11 @@
+# Import from Module
+from mymodule import my_func
+
+# Import from package
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+my_func()
+some_main_script.report_main()
+mysubscript.sub_report()
+
