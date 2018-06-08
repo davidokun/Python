8a174e9a72112f820de287e57fafbffc033b944e
davidokun <davidokun99@gmail.com>
2018-06-08 11:19:59 -0500

    Overview of Pylint

---
+++ 10-pylint-unittest/simple1.py
@@ -0,0 +1,19 @@
+# Install Pylint =  pip install pylint
+"""
+A very Simple Script
+"""
+
+
+def my_func():
+    """
+    A simple function
+    """
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+
+my_func()
+
+# Run: pylint simple1.py
