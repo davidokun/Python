28144b8ab0f8a342d7c0f7a0c9472f3c30afbc66
davidokun <davidokun99@gmail.com>
2018-06-08 15:48:57 -0500

    Python unit tests

---
+++ 10-pylint-unittest/2-unit-test/cap.py
@@ -0,0 +1,12 @@
+"""
+Simple class to capitalize a string
+"""
+
+
+def cap_text(text):
+    """
+    Capitalize first letter of a string
+    :param text: input string
+    :return: capitalized string
+    """
+    return text.title()
