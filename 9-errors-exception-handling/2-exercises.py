1862826707b3070911db35dcb4887b85a3525bf1
davidokun <davidokun99@gmail.com>
2018-06-08 09:03:40 -0500

    exercises: Problem 2. Use Finally clause.

--- 9-errors-exception-handling/2-exercises.py
+++ 9-errors-exception-handling/2-exercises.py
@@ -1,4 +1,4 @@
-# Problem 1
+print("\n# Problem 1\n")
 # Handle the exception thrown by the code below by using try and except blocks.
 
 
@@ -17,4 +17,30 @@ def exponential(numbers):
 exponential(['a', 'b', 'c'])
 exponential([2, 3, 4])
 exponential([2, "Hi", 4])
+
+print("\n# Problem 2\n")
+# Handle the exception thrown by the code below by using try and except blocks.
+# Then use a finally block to print 'All Done.'
+
+
+def divide(n1, n2):
+    """
+    Divide two numbers
+    :param n1: number 1
+    :param n2: number 2
+    """
+    try:
+        result = n1 / n2
+        print(f"Result is {result}")
+    except TypeError:
+        print(f"One value is not a number")
+    except ZeroDivisionError:
+        print(f"Can't divide by zero'")
+    finally:
+        print(f"All Done\n")
+
+
+divide(10, 5)
+divide(10, "5")
+divide(10, 0)
 
