5d659ce2109abbff46338ab3a4d208f5e66bc41c
davidokun <davidokun99@gmail.com>
2018-06-12 10:24:24 -0500

    milestone-2: Final game logic

--- 11-milestone-project-2/game.py
+++ 11-milestone-project-2/game.py
@@ -76,15 +76,15 @@ class Chips:
 
 # Game Logic functions
 
-def take_bet(chips, player_bet):
+def take_bet(chips):
     while True:
         try:
-            chips.bet = int(player_bet)  # int(input("How many chips would you like to bet?: "))
+            chips.bet = int(input("How many chips would you like to bet?: "))
         except ValueError:
-            return "Sorry, please provide a valid integer"
+            print("Sorry, please provide a valid integer")
         else:
             if chips.bet > chips.total:
-                return "Sorry, you do not have enough chips!. You have {}".format(chips.total)
+                print("Sorry, you do not have enough chips!. You have {}".format(chips.total))
             else:
                 break
 
@@ -95,11 +95,11 @@ def hit(deck, hand):
     hand.adjust_for_ace()
 
 
-def hit_or_stand(deck, hand, selection):
+def hit_or_stand(deck, hand):
     global playing
 
     while True:
-        x = selection  # input("Hit or Stand? Enter h or s: ")
+        x = input("Hit or Stand? Enter h or s: ")
 
         if x[0].lower() == 'h':
             hit(deck, hand)
@@ -111,6 +111,20 @@ def hit_or_stand(deck, hand, selection):
             continue
 
         break
+
+
+def show_some(player, dealer):
+    print("\nDealer's Hand:")
+    print(" <card hidden>")
+    print('',dealer.cards[1])
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+
+
+def show_all(player, dealer):
+    print("\nDealer's Hand:", *dealer.cards, sep='\n ')
+    print("Dealer's Hand =", dealer.value)
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    print("Player's Hand =", player.value)
 
 
 # End game function scenarios
@@ -137,3 +151,65 @@ def dealer_wins(player, dealer, chips):
 
 def push(player, dealer):
     print("Dealer and player tie! PUSH")
+
+
+# GAME LOGIC
+
+while True:
+
+    print("Welcome to BLACKJACK")
+
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    player_chips = Chips()
+
+    take_bet(player_chips)
+
+    show_some(player_hand, dealer_hand)
+
+    while playing:
+
+        hit_or_stand(deck, player_hand)
+        show_some(player_hand, dealer_hand)
+
+        if player_hand.value > 21:
+            player_busts(player_hand, dealer_hand, player_chips)
+
+            break
+
+    if player_hand.value <= 21:
+
+        while dealer_hand.value < player_hand.value:
+            hit(deck, dealer_hand)
+
+        show_all(player_hand, dealer_hand)
+
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+        else:
+            push(player_hand, dealer_hand)
+
+    print("\n Player total chips are at {}". format(player_chips.total))
+
+    new_game = input("Would you like to play another hand? y/n")
+
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print("Thank you for playing!")
+
+        break
