b5cb6cac09489ffb5f120a6f1acfff7a76bbcb27
davidokun <davidokun99@gmail.com>
2018-06-12 08:43:41 -0500

    milestone-2: Chips class definition

--- 11-milestone-project-2/game.py
+++ 11-milestone-project-2/game.py
@@ -59,3 +59,16 @@ class Hand:
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1
+
+
+class Chips:
+
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
