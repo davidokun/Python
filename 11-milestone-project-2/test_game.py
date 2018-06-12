7070445c5ff582f793048e23d7a639681d8f9deb
davidokun <davidokun99@gmail.com>
2018-06-12 08:39:30 -0500

    milestone-2: Unit tests

--- 11-milestone-project-2/test_game.py
+++ 11-milestone-project-2/test_game.py
@@ -8,3 +8,15 @@ class TestGame(unittest.TestCase):
         test_deck = game.Deck()
         self.assertEqual(len(test_deck.deck), 52)
 
+    def test_deal_to_player(self):
+        test_deck = game.Deck()
+        test_deck.shuffle()
+
+        test_player = game.Hand()
+        pulled_card = test_deck.deal()
+        test_player.add_card(pulled_card)
+
+        self.assertIsNotNone(test_player.value)
+        self.assertEquals(test_player.cards[0].suit, pulled_card.suit)
+
+
