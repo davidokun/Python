c2026f01b616a60b4fb0cb5be68c1dbbda5859cd
davidokun <davidokun99@gmail.com>
2018-06-18 08:12:43 -0500

    Advance String functions

---
+++ 15-advance-objects-and-data-structures/2-advance-strings.py
@@ -0,0 +1,30 @@
+# String functions
+
+s = 'hello world'
+
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+print(s.count('o'))
+print(s.find('o'))
+print(s.center(20, 'z'))
+
+print("hello\thi".expandtabs())
+
+s = 'Hello'
+print(s.isalnum())
+print(s.isalpha())
+
+print(s.islower())
+print(s.isspace())
+print(s.istitle())
+print(s.isupper())
+print(s.endswith('o'))
+
+# Regex
+split = s.split('e')
+print(split)
+
+s = 'hiihhihihihhhi'
+partition = s.partition('i')
+print(partition)
