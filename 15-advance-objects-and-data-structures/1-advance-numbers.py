d3f43613f16a9bc444b801adec491242888ae2b1
davidokun <davidokun99@gmail.com>
2018-06-18 08:02:47 -0500

    Advance mathematical functions

---
+++ 15-advance-objects-and-data-structures/1-advance-numbers.py
@@ -0,0 +1,21 @@
+# Hexadecimal
+print(hex(12))
+print(hex(512))
+
+# Binary
+print(bin(128))
+print(bin(512))
+
+# Exponential
+print(pow(2, 4))
+print(pow(2, 4, 3))
+
+# Absolute value
+print(abs(2))
+
+# Round
+print(round(3.6))  # Round up
+print(round(3.4))  # Round down
+print(round(3.4, 2))  # Round down
+
+
