fefde582c9a96f1c986bd61bcb09aa65694d5d30
davidokun <davidokun99@gmail.com>
2018-06-17 20:18:12 -0500

    Timeit module for measure code execution time

---
+++ 14-advance-python-modules/7-timeit.py
@@ -0,0 +1,10 @@
+import timeit
+
+t = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(t)
+
+t = timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000)
+print(t)
+
+t = timeit.timeit('"-".join(map(str, range(100)))', number=10000)
+print(t)
