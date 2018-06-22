b52a518b348d9134b2e452e91726f85b8e26ced8
davidokun <davidokun99@gmail.com>
2018-06-22 08:40:47 -0500

    Capstone: Recursive Factorial calculation.

--- 16-capstone-projects/numbers/factorial.py
+++ 16-capstone-projects/numbers/factorial.py
@@ -10,6 +10,9 @@ def factorial_iterative(num):
     :param num: the number to calculate the factorial
     :return: the factorial result
     """
+    if num == 0:
+        return 1
+
     factorial = 1
     for i in range(1, num + 1):
         factorial = factorial * i
@@ -17,14 +20,33 @@ def factorial_iterative(num):
     return factorial
 
 
+def factorial_recursive(num):
+    """
+    Calculates factorial of a given number
+    Uses Recursive approach
+    :param num: the number to calculate the factorial
+    :return: the factorial result
+    """
+    if num == 0:
+        return 1
+
+    return num * factorial_recursive(num - 1)
+
+
 if __name__ == '__main__':
 
     while True:
         try:
-            VALUE = int(input("Select option to get Factorial:\n* 1-Iterative\n=> "))
+            VALUE = int(input("Select option to get Factorial:\n* 1-Iterative\n* 2-Recursive\n=> "))
+
             if VALUE == 1:
-                NUMBER = int(input("Enter positive integer: "))
-                print(f'Factorial of {NUMBER} is : {factorial_iterative(7)}\n')
+                NUMBER = int(input("(Iterative) Enter positive integer: "))
+                print(f'Factorial of {NUMBER} is : {factorial_iterative(NUMBER)}\n')
+
+            if VALUE == 2:
+                NUMBER = int(input("(Recursive) Enter positive integer: "))
+                print(f'Factorial of {NUMBER} is : {factorial_recursive(NUMBER)}\n')
+
         except ValueError:
             print('Bye')
             break
